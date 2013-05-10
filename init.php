5e40239951476038033eb6e12900233f93884e58
Staicu Ionut <github@iamntz.com>
2013-05-10 20:52:33 +0300

    added sort by date

--- init.php
+++ init.php
@@ -24,28 +24,37 @@ class Ntz_Mini_Agregator{
     ), $atts ) );
 
     if( $rss_feeds ){
+      include_once( ABSPATH . WPINC . '/feed.php' );
+
       $blogs = array();
 
-      include_once( ABSPATH . WPINC . '/feed.php' );
       $rss_feeds = explode( "\n", $rss_feeds );
+
       foreach( (array) $rss_feeds as $rss_feed ){
         $rss_feed = strip_tags( $rss_feed );
         if( !empty( $rss_feed ) ){
           $rss = fetch_feed( $rss_feed );
           if ( !is_wp_error( $rss ) ){
             $maxitems = $rss->get_item_quantity( 5 );
-            $blogs[] = $rss->get_items( 0, $maxitems );
+            $feed = $rss->get_items( 0, $maxitems );
+            $blogs[] = array(
+              "feed" => $feed,
+              "date" => strtotime( $feed[0]->get_date() )
+            );
           }
         }
       }
 
+      usort( $blogs, array( $this, 'sort_rss_by_date' ) );
+
       foreach( (array) $blogs as $rss_items ){
         $parsed_rss .= '<ul>';
-        $parsed_rss .= sprintf( '<li><h2>%s</h2></li>',
-          $rss_items[0]->get_feed()->get_title()
+        $parsed_rss .= sprintf( '<li><h2>%s - %s</h2></li>',
+          $rss_items['feed'][0]->get_feed()->get_title(),
+          date( 'M j, Y', $rss_items['date'])
         );
 
-        foreach( $rss_items as $rss_item ){
+        foreach( $rss_items['feed'] as $rss_item ){
             $parsed_rss .= '<li>';
             $parsed_rss .= sprintf( '<a href="%s">%s</a>', 
               $rss_item->get_permalink(),
@@ -58,6 +67,11 @@ class Ntz_Mini_Agregator{
 
     return $parsed_rss;
   } // add_shortcode
+
+
+  public function sort_rss_by_date( $b, $a ){
+    return strcmp($a['date'], $b['date']);
+  } // sort_rss_by_date
 }
 
 new Ntz_Mini_Agregator();
